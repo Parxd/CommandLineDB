462af86969f06b7f18f17fc27255536648852a2b
parxed <daniel.j.park@vanderbilt.edu>
2023-07-06 21:07:09 -0700

    verify user input for connect.cpp

--- src/cmds/connect.cpp
+++ src/cmds/connect.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 #include <utility>
 #include <filesystem>
-#include <boost/filesystem.hpp>
 #include "../../include/cmds/connect.h"
 
-Connect::Connect(std::string args): filepath(std::move(args)) {
-    if (!std::filesystem::exists(filepath)) {
-        throw std::invalid_argument("Invalid format - formatting or file path is invalid.");
+Connect::Connect(std::string args) {
+    if (std::filesystem::is_regular_file(args)) {
+        // still need to check if it's a json file and a valid one at that too
+        filepath = std::move(args);
     }
-    boost::filesystem::path filePath(filepath);
-    if (boost::filesystem::exists(filePath)) {
-        std::cout << "hello";
+    else {
+        auto curPath = std::string(std::filesystem::current_path());
+        if (args.front() == '/') {
+            curPath += args;
+        }
+        else {
+            curPath += "/" + args;
+        }
+        if (std::filesystem::is_regular_file(curPath)) {
+            // still need to check if it's a json file and a valid one at that too
+            filepath = std::move(curPath);
+        }
+        else {
+            throw std::invalid_argument("Invalid format - formatting or file path is invalid.");
+        }
     }
+    std::cout << "Connected to persistent database file at " + filepath << "\n";
 }
 bool Connect::execute() {
     return false;
